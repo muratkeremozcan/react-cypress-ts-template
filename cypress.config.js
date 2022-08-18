bfcb875e98b4f1c55ca9f6de915c50cd665eaa94
Murat Ozcan <muratkerem@gmail.com>
2022-08-18 10:49:48 -0500

    types

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
-    // baseUrl, etc
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // and load any plugins that require the Node environment
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 })
