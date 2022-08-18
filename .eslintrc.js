4ea5cb381f4e696a80391f1d5914ec5e4ffbaef2
Murat Ozcan <muratkerem@gmail.com>
2022-08-18 07:39:14 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "chai-friendly", "no-only-tests"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "no-only-tests/no-only-tests": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-var-requires": "off",
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+};
